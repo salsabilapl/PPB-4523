868c7c3fed027f7509e9a52c137933b0fe0bc57e
salsabilapl <lesmarnasalsabila@gmail.com>
2021-10-01 20:17:01 +0700

    add tugas

--- Tugas 2/Aplikasi Toast/app/src/main/java/com/example/aplikasitoast/MainActivity.java
+++ Tugas 2/Aplikasi Toast/app/src/main/java/com/example/aplikasitoast/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.aplikasitoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView angkacounter;
+    int angka=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        angkacounter = findViewById(R.id.kounter);
+    }
+
+    public void toastklik(View view) {
+        Toast.makeText(this, "INI ADALAH TOAST", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void tb1kounter(View view) {
+        angka=angka+1;
+        angkacounter.setText(Integer.toString(angka));
     }
 }
